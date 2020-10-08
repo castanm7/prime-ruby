767863210ee10d07b73c557c41566cfc58653190
Maria Castaneda <mariamcastaneda29@gmail.com>
2020-10-07 22:24:51 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+
+def prime?(x)
+   return false if !x.integer?
+   return false if x < 2
+   return true if x == 2
+   range = (2..x-1)
+   range.each {|i| return false if x % i == 0}
+   true
+end
+
